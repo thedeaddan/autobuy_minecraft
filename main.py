266605a8516417c2eb959d56cfc1a4a9802c0f39
Даниил <40400854+thedeaddan@users.noreply.github.com>
2022-07-27 00:55:51 +0300

    Уведомления в ТГ и очень сильное ускорение работы

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from pyautogui import moveTo,click #Импортируем из библиотеки мыши функцию клика и движения
-from keyboard import press,release #Из библиотеки управления клавиатурой импортируем нажатие и отпускание клавиши
-from sys import exit #Импортируем системную либу для функции выхода
-from time import sleep #Из библиотеки времени импортируем функию ожидания
-from python_imagesearch.imagesearch import imagesearch #Из библиотеки машинного зрения импортируем функцию поиска по шаблону
-from win32gui import GetWindowText, GetForegroundWindow #Из библиотеки UI Windows импортируем функции имени окна
+from pyautogui import moveTo,click,screenshot # Подключения из библиотеки функций движения мыши, клика, и создания скриншота
+from keyboard import press,release #Подключения функций зажатия и отпускания клавиши клавиатуры
+from sys import exit #функция выхода
+from time import sleep #функция задержки
+from python_imagesearch.imagesearch import imagesearcharea #Поиск в определенной области
+from win32gui import GetWindowText, GetForegroundWindow #Получение названия окна
+import threading #Многопоточность
+import telebot #Bot API Telegram
 
-#Задаем название окна
-window = "MatHax v1.7.6 - modified/Fabric 1.18.1"
+window = "MatHax v1.7.6 - modified/Fabric 1.18.1" #Название окна игры
+
+bot = telebot.TeleBot("token") #Вместо token ваш токен от бота
+chat_id = "@chatid" #ID чата куда кидать покупки
+user = "thede" #Имя вашего пользователя 
+
+def screen():
+	sleep(1) #Ожидание 1 сек.
+	f = open(f'C:/Users/{user}/AppData/Roaming/.minecraft/logs/latest.log','r+') #Открытие логов
+	lines = f.readlines() #Чтение логов
+	f.close() #Закрытие логов
+	lens = len(lines) #Получения количества строк
+	item = "" #Создания переменной для предмета который купили
+	while True: #Бесконечный цикл
+		fined = lines[lens-1] #Получение с последней до первой строки логов 
+		if "Этот товар уже купили!" in fined or "У Вас не хватает денег!" in fined: #Проверка 
+			break #Окончить цикл
+		if "Вы успешно купили" in fined: #Если есть уведомление о покупке
+			item = fined.split("купили")[1].replace("\n","")[1:] #Получить название того, что купили
+			break #Окончание цикла
+		lens -= 1 #переход к предыдущей строке 
+ 
+	if item != "": #Если предмет есть то
+		print(f"Купил {item}") #Отладка
+		bot.send_photo(chat_id,screensh,f"Купил {item}") #Отправка скриншота в чат с названием того что купили
+
 
 #Цикл, ожидающий открытия игры
 while True:
@@ -24,46 +50,50 @@ def antiafk():
 		sleep(0.1)
 		#Отпускаем
 		release(i)
-#Главный цикл
+
+#Бесконечный цикл 
 while True:
-	try:	#Проверка окна 
+	try:
+		#Проверка на текущее окно
 		if GetWindowText(GetForegroundWindow()) == window:
-			i = 821 #Начальная координата клетки по горизонтали
-			g = 0 #Номер клетки
+			i = 1132 #Начальная координата клетки по горизонтали
+			g = 0 #Номер ячейки
 			press("shift") #Зажимание шифта, так быстрее покупает, не нужно подтверджать
-			# 7 - Количество проверяемых ячеек
-			while g != 7:
+			while g != 6:
 				#Проверка какое сейчас окно и если оно игра, то запускаем
 				if GetWindowText(GetForegroundWindow()) == window:
 					#передвижение на клетку
-					moveTo(i,352)
-					#Ищем на экране совпадение с шаблоном
-					pos = imagesearch("./1.png")
+					moveTo(i,538)
+					#Ищем в области по координатам совпадение с шаблоном
+					pos = imagesearcharea("./1.png",1112,554,1503,731)
 					#если совпадения есть
 					if pos[0] != -1:
-						#Уведомляем и покупаем
-						print("Вижу предмет за 10$ : ", pos[0], pos[1])
+						#Создаем глобальную переменную со скриншотом инвентаря
+						global screensh
+						screensh = screenshot('screenshot.png',region=(1110,510,500,500))
+						#Кликаем по предмету
 						click()
+						#Запускаем поток с отправкой уведомления
+						threading.Thread(target = screen).start()
 					#Добавляем координату по горизонтали, и меняем номер клетки
-					i += 35
+					i += 37
 					g += 1
 				else:
 					#Если окно не игра, отпускаем на всякий случай шифт(Были случаи зажима) и выходим из скрипта
 					release("shift")
 					exit()
-			#Передвигаемся на кнопку обновления
-			moveTo(963,535)
-			#Отжимаем шифт(Так надо, в ином случае созда			
-			release("shift")
-			#Кликаем
-			click()
+
+			moveTo(1277,716)#Передвигаемся на кнопку обновления
+			release("shift")#Отжимаем шифт(Так надо, в ином случае создаст проблемы)
+			click()#Кликаем
+
 		else:
 			#Та же проверка на текущее окна, с шифтом и выходом
 			release("shift")
 			exit()
-	#Если нажато CTRL+C
+	#Если нажато CTRL+C			
 	except KeyboardInterrupt:
-		#Отжимаем шифт и выходим
+		#Та же проверка на текущее окна, с шифтом и выходом
 		release("shift")
 		exit()
 
